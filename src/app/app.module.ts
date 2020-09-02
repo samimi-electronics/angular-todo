8285206767bb1ea88ac5e6bd4735b86ed2be97cd
Hamid Samimi <samimi.electronics@gmail.com>
2020-09-02 15:18:59 +0430

    added add-todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
