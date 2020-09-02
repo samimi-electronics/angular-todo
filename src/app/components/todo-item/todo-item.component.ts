372f8cda78420eb24ee3d86abeca956c874cdcf7
Hamid Samimi <samimi.electronics@gmail.com>
2020-09-02 13:30:21 +0430

    fixed bad classname

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -18,7 +18,7 @@ export class TodoItemComponent implements OnInit {
   setClasses() {
     let classes = {
       todo: true,
-      'is-completed': this.todo.completed
+      'is-complete': this.todo.completed
     }
     return classes;
   }
