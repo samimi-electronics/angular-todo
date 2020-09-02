5f7de3da7fbbf15d0de4aa22d0b8f924f297ad66
Hamid Samimi <samimi.electronics@gmail.com>
2020-09-02 15:35:57 +0430

    implemented add-todo component

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
   styleUrls: ['./add-todo.component.css']
 })
 export class AddTodoComponent implements OnInit {
+
+  title: string
 
   constructor() { }
 
