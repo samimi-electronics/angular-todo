219a80e72e62c3dda7b91fa4ad606221500a1a43
Hamid Samimi <samimi.electronics@gmail.com>
2020-09-02 13:42:23 +0430

    moved todos data to todos.service

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -12,23 +12,7 @@ export class TodosComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo #1',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo #2',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo #3',
-        completed: false
-      },
-    ]
+    this.todos = 
   }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class TodoService {
 
   constructor() { }
+
+  // Get all todos
+  getTodos() {
+    return [
+      {
+        id: 1,
+        title: 'Todo #1',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo #2',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo #3',
+        completed: false
+      },
+    ]
+  }
 }
