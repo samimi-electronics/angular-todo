de8a461880634a2e52ab9001bc02c5b79effff82
Hamid Samimi <samimi.electronics@gmail.com>
2020-09-02 14:06:23 +0430

    finished up todos.service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,31 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-
+import { Todo } from '../models/Todo'
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosURL: 'https://jsonplaceholder.typicode.com/todos'
 
   constructor(private http: HttpClient) { }
 
   // Get all todos
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Todo #1',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo #2',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo #3',
-        completed: false
-      },
-    ]
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.todosURL, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   }
 }
